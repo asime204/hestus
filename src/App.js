07876f17db9579e76c2d70e42522d3fd8a0963d2
Haze <hazecode25@gmail.com>
2024-04-17 22:04:25 -0400

    Home/landing page created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
+import { Routes, Route, BrowserRouter as Router, redirect } from 'react-router-dom';
+// import Nav from './components/Nav';
+import Home from './views/Home';
+// import SignUp from './views/Signup';
+// import Login from './views/Login';
+// import Feed from './views/Feed';
+// import Budget from './views/Budget';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App() {
+    // const getUserFromLocalStorage = () => {
+    //     const foundUser = localStorage.getItem('user107')
+    //     if (foundUser){
+    //         return JSON.parse(foundUser)
+    //     }
+    //     return {}
+    // };
+
+    // const logMeIn = (user) => {
+    //         setUser(user);
+    //         localStorage.setItem('user107', JSON.stringify(user))
+    //     };
+    // const logMeOut = () => {
+    //     setUser({});
+    //     localStorage.removeItem('user107')
+    // };
+
+    // const showMessage = () => {
+    //     return messages.map(m => <p>{m}</p>)
+    // }
+
+    // const [user, setUser] = useState(getUserFromLocalStorage());
+    // const [messages, setMessages] = useState([]);
+    // const [paychecks, setPaychecks] = useState([]);
+    // const [bills, setBills] = useState([]);
+    // const [leftOver, setLeftOver] = useState('');
+
+
+    // const handleAddPaycheck = () => {
+    //     const newPaycheck = { title: '', amount: '', trans_date: '' };
+    //     setPaychecks([...paychecks, newPaycheck]);
+    //     console.log('Paycheck added:', paychecks);
+    // };
+
+    // const handleAddBill = () => {
+    //     const newBill = { title: '', amount: '', trans_date: '' };
+    //     setBills([...bills, newBill]);
+    //     console.log('Bill added:', bills);
+    // };
+
+    // const totalIncome = paychecks.reduce(
+    //     (accumulator, paycheck) => accumulator + Number(paycheck.amount),
+    //     0
+    //   );
+
+    //   const totalBills = bills.reduce(
+    //     (accumulator, bill) => accumulator + Number(bill.amount),
+    //     0
+    //   );
+
+    // const calculateLeftOver = () => {
+    //     return totalIncome - totalBills;
+    // };
+
+
+    // useEffect(() => {
+    //     const fetchPaychecks = async () => {
+    //       try {
+    //         const response = await fetch(`http://localhost:5000/api/budget/paychecks`, {
+    //           headers: {
+    //             Authorization: `Bearer ${user.apitoken}`,
+    //           },
+    //         });
+    //         const data = await response.json();
+    //         if (data.paychecks) {
+    //           setPaychecks(data.paychecks);
+    //         }
+    //       } catch (error) {
+    //         console.error(error);
+    //       }
+    //     };
+    //     fetchPaychecks();
+    //   }, [user]);
+
+    // useEffect(() => {
+    // const fetchBills = async () => {
+    //     try {
+    //     const response = await fetch(`http://localhost:5000/api/budget/bills`, {
+    //         headers: {
+    //         Authorization: `Bearer ${user.apitoken}`,
+    //         },
+    //     });
+    //     const data = await response.json();
+    //     if (data.bills) {
+    //         setBills(data.bills);
+    //     }
+    //     } catch (error) {
+    //     console.error(error);
+    //     }
+    // };
+    // fetchBills();
+    // }, [user]);
+
+
+    return (
+        <Router>
+            <div style={{ backgroundColor: '#e6e6fa' }}>
+                {/* <Nav user={user} logMeOut={logMeOut} />
+                {showMessage()} */}
+
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        {/* <Route path='/budget' element={<Budget user={user} paychecks={paychecks} setPaychecks={setPaychecks} bills={bills} setBills={setBills} setLeftOver={setLeftOver} calculateLeftOver={calculateLeftOver} handleAddPaycheck={handleAddPaycheck} handleAddBill={handleAddBill}/>} />
+                        <Route path='/feed' element={<Feed totalIncome={totalIncome} totalBills={totalBills} paychecks={paychecks} bills={bills}/>} />
+                        <Route path='/signup' element={<SignUp />} />
+                        <Route path='/login' element={<Login logMeIn={logMeIn}/>} /> */}
+                        {/* <Route path='/posts/:postId' element={<SinglePost user={user}/>} />
+                        <Route path='/posts/update/:postId' element={<UpdatePost user={user}/>} />
+                        <Route path='/posts/create' element={<CreatePost user={user}/>} /> */}
+                    </Routes>
+
+
+            </div>
+        </Router>
+    )
+
 }
-
-export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+// import '../css/main.css';
+
+function Home(props) {
+  const [textLoaded, setTextLoaded] = useState(false);
+  const [buttonsLoaded, setButtonsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTextLoaded(true);
+    setTimeout(() => {
+      setButtonsLoaded(true);
+    }, 1000);
+  }, []);
+
+  return (
+    <div className='body'>
+      <h1 className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Hestus</h1>
+      {/* <h3 className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Your personal budget financial planner</h3> */}
+      <p className={`fade-in ${textLoaded ? 'loaded' : ''}`}>Get started by creating an account or logging in to your existing account.</p>
+      <div className="card">
+        <div className={`card-body ${buttonsLoaded ? 'loaded' : ''}`}>
+          <button className="homebutton"><a href="/signup">Create Account</a></button>
+          <button className="homebutton"><a href="/login">Log In</a></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
