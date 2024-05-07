7310dd6a1685f92ba7dab2e15d0b8770700d5c5a
haze - mac <hazecode25@gmail.com>
2024-05-07 10:33:30 -0400

    Added .env file

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import { getAnalytics } from "firebase/analytics";
 import './css/index.css';
 import App from './App';
 
+
+const REACT_APP_FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID
+const REACT_APP_FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
+// const REACT_APP_FIREBASE_DATABASE_URL = process.env.REACT_APP_FIREBASE_DATABASE_URL
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA9g5qOHM9G8vTKqcwjY4GDIfOueJuCDuo",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: "hestus-943dc.firebaseapp.com",
   projectId: "hestus-943dc",
   storageBucket: "hestus-943dc.appspot.com",
   messagingSenderId: "986610836419",
-  appId: "1:986610836419:web:51cf088c10488b23eed376",
+  appId: REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-4L2DLWN5NB"
 };
 
