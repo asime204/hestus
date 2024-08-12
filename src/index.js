e2c128d26ede7573db8c99f9de662dd34b2c367a
haze - mac <hazecode25@gmail.com>
2024-08-12 19:40:31 -0400

    secured Firebase config

--- src/index.js
+++ src/index.js
@@ -8,21 +8,27 @@ import App from './App';
 
 const REACT_APP_FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID
 const REACT_APP_FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
+const REACT_APP_FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+const REACT_APP_FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID
+const REACT_APP_FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+const REACT_APP_FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+const REACT_APP_FIREBASE_MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 // const REACT_APP_FIREBASE_DATABASE_URL = process.env.REACT_APP_FIREBASE_DATABASE_URL
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: "hestus-943dc.firebaseapp.com",
-  projectId: "hestus-943dc",
-  storageBucket: "hestus-943dc.appspot.com",
-  messagingSenderId: "986610836419",
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-4L2DLWN5NB"
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
